64ed8c14cee9261da3401027171128810cb3973b
griegomas <38501062+griegomas@users.noreply.github.com>
2018-11-18 22:01:17 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,31 @@
 (function($){
   $(function(){
    
-
+    $('.button-collapse').sideNav();
     
+  
+
+    $(".side-link a").click(function(e) {
+        e.preventDefault();
+        $(".side-link a").removeClass('active-side-link');
+        $(this).addClass('active-side-link');
+
+        var elemToScroll = $(e.target).attr('link');
+        $('html, body').animate({
+            scrollTop: $(elemToScroll).offset().top
+        }, 500);
+    });
+    
+    
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
     $("#contact_form_submit").click(function(e){
         e.preventDefault();
         var name = $("#contact_name").val();
