8b214289d5e85da0161f7db078cc3e69aa50d6f1
griegomas <38501062+griegomas@users.noreply.github.com>
2018-11-19 20:42:47 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,7 @@
 (function($){
   $(function(){
    
-    $('.button-collapse').sideNav();
-    
-  
-
-    $(".side-link a").click(function(e) {
-        e.preventDefault();
-        $(".side-link a").removeClass('active-side-link');
-        $(this).addClass('active-side-link');
-
-        var elemToScroll = $(e.target).attr('link');
-        $('html, body').animate({
-            scrollTop: $(elemToScroll).offset().top
-        }, 500);
-    });
+   
     
     
   document.addEventListener('DOMContentLoaded', function() {
@@ -26,28 +13,12 @@
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, options);
   });
-    $("#contact_form_submit").click(function(e){
-        e.preventDefault();
-        var name = $("#contact_name").val();
-        var email = $("#contact_email").val();
-        var msg = $("#contact_message").val();
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+    
+  });
 
-        $.ajax({
-          type: "POST",
-          url: "contact.php",
-          data: {
-            'name':name,
-            'email':email,
-            'message':msg,
-          },
-          success: function(data){
-            $("#contact_name").val("");
-            $("#contact_email").val("");
-            $("#contact_message").val("");
-            $("#contact-success").openModal();
-          },
-        });
-    });
     
 
 
